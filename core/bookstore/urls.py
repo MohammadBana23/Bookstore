da38d7cd8650bbaf17693817d2d030fb468601f7
Mohammad Bana <mohamad.bana.1380@gmial.com>
2024-04-20 02:28:35 +0330

    add retrieve download link of book endpoint

--- core/bookstore/urls.py
+++ core/bookstore/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from bookstore.views import (
-    SignUpView, LoginView, RequestTokenView, ChargeAccountView
+    SignUpView, LoginView, RequestTokenView, ChargeAccountView,
+    BuyBookCreateGenericAPIView, BookDownloadAPIView
 )
 
 urlpatterns = [
     path('signup/' , SignUpView.as_view(), name='signup'),
     path('login/' , LoginView.as_view(), name='login'),
     path("request/token/", RequestTokenView.as_view(), name='request-token'),
-    path("charge-account/", ChargeAccountView.as_view(), name='charge-account')
+    path("charge-account/", ChargeAccountView.as_view(), name='charge-account'),
+    path("buy-book/", BuyBookCreateGenericAPIView.as_view(), name='buy-book'),
+    path("book/<int:pk>/download/", BookDownloadAPIView.as_view(), name='book-retrieve'),
 ]
