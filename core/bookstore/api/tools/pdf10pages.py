72ca575b9257f6f721c726a00300aa75c117e583
Mohammad Bana <mohamad.bana.1380@gmial.com>
2024-05-11 17:30:08 +0330

    upload and get download link of books from minio server

--- core/bookstore/api/tools/pdf10pages.py
+++ core/bookstore/api/tools/pdf10pages.py
@@ -8,8 +8,9 @@ def save_ten_pages_pdf(pdf_path):
     output_pdf = PdfWriter()
     for page_num in range(min(10, len(input_pdf.pages))):
         output_pdf.add_page(input_pdf.pages[page_num])
-
-    output_file_path = os.path.join(settings.MEDIA_ROOT, 'book_10pages', 'Django for Beginners-10pages.pdf')
+        
+    pdf_file_path = pdf_path.split("/")[-1]
+    output_file_path = os.path.join(settings.MEDIA_ROOT, 'book_10pages', f'{pdf_file_path.split(".")[0]}-10pages.pdf')
     os.makedirs(os.path.dirname(output_file_path), exist_ok=True)
 
     with open(output_file_path, 'wb') as output_file:
